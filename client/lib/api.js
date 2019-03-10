abe56b7d108e639b516ee703bf2b0268477c98fe
taokexia <694198149@qq.com>
2019-03-10 23:50:31 +0800

    data commit

--- client/lib/api.js
+++ client/lib/api.js
@@ -1,8 +1,29 @@
+import { geocoder } from '../../lib/api'
 // 填写 env
 wx.cloud.init({
   env: ''
 })
 
+// 地图 key
+const QQ_MAP_KEY = 'NZ4BZ-WCC36-5LDS3-ECV7V-CB5CO-HIBIP'
+
+/**
+ *  逆经纬度查询
+ * @param {*} lat
+ * @param {*} lon
+ */
+export const geocoder = (lat, lon, success = () => {}, fail = () => {}) => {
+  return wx.request({
+    url: 'https://apis.map.qq.com/ws/geocoder/v1/',
+    data: {
+      location: `${lat},${lon}`,
+      key: QQ_MAP_KEY,
+      get_poi: 0
+    },
+    success,
+    fail
+  })
+}
 
 export const test = () => {
   return wx.cloud.callFunction({
--- client/pages/weather/index.js
+++ client/pages/weather/index.js
@@ -28,7 +28,76 @@ Page({
       temp: "24/30",
       icon: "leizhenyu",
       weather: "雷阵雨"
-    }
+    },
+    hourlyData: [
+      {
+        temp: 29,
+        time: "16:00",
+        weather: "雷阵雨",
+        icon: "leizhengyu"
+      },
+      {
+        temp: 29,
+        time: "16:00",
+        weather: "雷阵雨",
+        icon: "leizhengyu"
+      },
+      {
+        temp: 29,
+        time: "16:00",
+        weather: "雷阵雨",
+        icon: "leizhengyu"
+      }
+    ],
+    weeklyData: [
+      {
+        day: "雷阵雨",
+        dayIcon: "leizhengyu",
+        dayWind: "南风",
+        dayWindLevel: "1-2",
+        maxTemp: "30",
+        minTemp: "24",
+        night: "中雨",
+        nightIcon: "zhenyuye",
+        ninghWind: "南风",
+        nightWindLevel: "1-2",
+        time: 1534032000000
+      },
+      {
+        day: "雷阵雨",
+        dayIcon: "leizhengyu",
+        dayWind: "南风",
+        dayWindLevel: "1-2",
+        maxTemp: "30",
+        minTemp: "24",
+        night: "中雨",
+        nightIcon: "zhenyuye",
+        ninghWind: "南风",
+        nightWindLevel: "1-2",
+        time: 1534032000000
+      },
+      {
+        day: "雷阵雨",
+        dayIcon: "leizhengyu",
+        dayWind: "南风",
+        dayWindLevel: "1-2",
+        maxTemp: "30",
+        minTemp: "24",
+        night: "中雨",
+        nightIcon: "zhenyuye",
+        ninghWind: "南风",
+        nightWindLevel: "1-2",
+        time: 1534032000000
+      }
+    ],
+    lifeStyle: [
+      {
+        name: "舒适度",
+        icon: "guominzhishu",
+        info: "较不舒适",
+        detail: "白天虽有有雨..."
+      }
+    ]
   },
   onLoad() {
     wx.getSystemInfo({
@@ -39,6 +108,76 @@ Page({
           paddingTop: res.statusBarHeight + 12
         })
       }
+    })
+    this.getLocation()
+  },
+  // 处理逆地址
+  getAddress(lat, lon, name) {
+    wx.showLoading({
+      title: '定位中',
+      mask: true
+    })
+    let fail = (e) => {
+      this.setData({
+        address: name || '北京市海淀区西二旗北路'
+      })
+      wx.hideLoading()
+      this.getWeatherData()
+    }
+    geocoder(
+      lat,
+      lon,
+      (res) => {
+        wx.hideLoading()
+        let result = (res.data || {}).result
+
+        if(res.statusCode === 200 && result.address) {
+          let {address, formatted_addresses, address_component} = result
+          if(formatted_addresses && (formatted_addresses.recommend || formatted_addresses.rough)) {
+            address = formatted_addresses.recommend || formatted_addresses.rough
+          }
+          let {province, city, district: county} = address_component
+          this.setData({
+            province,
+            county,
+            city,
+            address: name || address
+          })
+          this.getWeatherData()
+        } else {
+          fail()
+        }
+      },
+      fail
+    )
+  },
+  updateLocation(res) {
+    let {latitude: lat, longitude: lon, name} = res
+    let data = {
+      lat,
+      lon
+    }
+    if(name) {
+      data.address = name
+    }
+    this.setData(data)
+    this.getAddress(lat, lon, name)
+  },
+  getLocation() {
+    wx.getLocation({
+      type: 'gcj02',
+      success: this.updateLocation,
+      fail: (e) => {
+        this.openLocation()
+      }
+    })
+  },
+  // 检测失败，提示用户
+  openLocation() {
+    wx.showToast({
+      title: '检测到您未授权使用位置权限，请先开启哦',
+      icon: 'none',
+      duration: 3000
     })
   }
 })
